42c536b505a9efe06ad554ffeaea827559244410
Carl Perry <carl.perry@dreamhost.com>
2012-11-15 08:18:09 -0800

    Config updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,27 @@ default["developer_mode"] = false  # we want secure passwords by default
 ########################################################################
 
 default["openstack"]["quantum"]["folsom"]["version"] = "2012.2+git201209242000~precise-0ubuntu1"
+
+default["openstack"]["quantum"]["services"]["api"]["scheme"] = "http"
+default["openstack"]["quantum"]["services"]["api"]["network"] = "public"
+default["openstack"]["quantum"]["services"]["api"]["port"] = 9696
+default["openstack"]["quantum"]["services"]["api"]["path"] = "/v2.0"
+
+default["quantum"]["services"]["api"]["scheme"] = "http"
+default["quantum"]["services"]["api"]["network"] = "public"
+default["quantum"]["services"]["api"]["port"] = 9696
+default["quantum"]["services"]["api"]["path"] = "/v2.0"
+
+default["openstack"]["quantum"]["db"]["name"] = "quantum"
+default["openstack"]["quantum"]["db"]["username"] = "quantum"
+
+# TODO: These may need to be glance-registry specific.. and looked up by glance-api
+default["openstack"]["quantum"]["service_tenant_name"] = "service"
+default["openstack"]["quantum"]["service_user"] = "quantum"
+default["openstack"]["quantum"]["service_role"] = "admin"
+
+# logging attribute
+default["openstack"]["quantum"]["syslog"]["use"] = false
+default["openstack"]["quantum"]["syslog"]["facility"] = "LOG_LOCAL2"
+default["openstack"]["quantum"]["syslog"]["config_facility"] = "local2"
+
--- recipes/server.rb
+++ recipes/server.rb
@@ -5,10 +5,40 @@
 # Copyright 2012, DreamHost
 #
 
-rabbit_ip = IPManagement.get_ips_for_role("rabbitmq-server", "nova", node)[0]
+rabbit_info = get_settings_by_role("rabbitmq-server", "rabbitmq") # FIXME: access
 keystone = get_settings_by_role("keystone", "keystone")
 ks_admin_endpoint = get_access_endpoint("keystone", "keystone", "admin-api")
 ks_service_endpoint = get_access_endpoint("keystone", "keystone", "service-api")
+api_endpoint = get_bind_endpoint("quantum", "api")
+
+# Allow for using a well known db password
+if node["developer_mode"]
+  node.set_unless["openstack"]["quantum"]["db"]["password"] = "quantum"
+else
+  node.set_unless["openstack"]["quantum"]["db"]["password"] = secure_password
+end
+
+# Allow for using a well known db password
+if node["developer_mode"]
+  node.set_unless["openstack"]["quantum"]["service_pass"] = "quantum"
+else
+  node.set_unless["openstack"]["quantum"]["service_pass"] = secure_password
+end
+
+# Register Service Tenant
+keystone_register "Register Quantum Service Tenant" do
+  auth_host ks_admin_endpoint["host"]
+  auth_port ks_admin_endpoint["port"]
+  auth_protocol ks_admin_endpoint["scheme"]
+  api_ver ks_admin_endpoint["path"]
+  auth_token keystone["admin_token"]
+  tenant_name node["openstack"]["quantum"]["service_tenant_name"]
+  user_name node["openstack"]["quantum"]["service_user"]
+  user_pass node["openstack"]["quantum"]["service_pass"]
+  tenant_description "Quantum Service Tenant"
+  tenant_enabled "true" # Not required as this is the default
+  action :create_user
+end
 
 package "quantum-server" do
   action :install
@@ -20,25 +50,57 @@ template "/etc/quantum/quantum.conf" do
   group "root"
   mode "0644"
   variables(
-            "rabbit_ipaddress" => rabbit_ip
-            )
+    "rabbit_ipaddress" => IPManagement.get_ips_for_role("rabbitmq-server","nova",node)[0]    #FIXME!
+  )
 end
 
 template "/etc/quantum/api-paste.ini" do
-    source "api-paste.ini.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    variables(
-      "component"  => node["package_component"],
-      "keystone_api_ipaddress" => ks_service_endpoint["host"],
-      "service_port" => ks_service_endpoint["port"],
-      "admin_port" => ks_admin_endpoint["port"],
-      "admin_token" => keystone["admin_token"]
-    )
+  source "api-paste.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    "component"  => node["package_component"],
+    "use_syslog" => node["openstack"]["quantum"]["syslog"]["use"],
+    "log_facility" => node["openstack"]["quantum"]["syslog"]["facility"],
+    "keystone_api_ipaddress" => ks_admin_endpoint["host"],
+    "keystone_service_port" => ks_service_endpoint["port"],
+    "keystone_admin_port" => ks_admin_endpoint["port"],
+    "keystone_admin_token" => keystone["admin_token"],
+    "service_tenant_name" => node["openstack"]["quantum"]["service_tenant_name"],
+    "service_user" => node["openstack"]["quantum"]["service_user"],
+    "service_pass" => node["openstack"]["quantum"]["service_pass"]
+  )
  notifies :restart, resources(:service => "nova-api-os-compute"), :delayed
 end
 
+# Register Network Service
+keystone_register "Register Network Service" do
+  auth_host ks_admin_endpoint["host"]
+  auth_port ks_admin_endpoint["port"]
+  auth_protocol ks_admin_endpoint["scheme"]
+  api_ver ks_admin_endpoint["path"]
+  auth_token keystone["admin_token"]
+  service_name "quantum"
+  service_type "network"
+  service_description "Quantum Network Service"
+  action :create_service
+end
+
+# Register Network Endpoint
+keystone_register "Register Network Endpoint" do
+  auth_host ks_admin_endpoint["host"]
+  auth_port ks_admin_endpoint["port"]
+  auth_protocol ks_admin_endpoint["scheme"]
+  api_ver ks_admin_endpoint["path"]
+  auth_token keystone["admin_token"]
+  service_type "network"
+  endpoint_region "RegionOne"
+  endpoint_adminurl api_endpoint["uri"]
+  endpoint_internalurl api_endpoint["uri"]
+  endpoint_publicurl api_endpoint["uri"]
+  action :create_endpoint
+end
 
 service "quantum-server" do
      action :restart
