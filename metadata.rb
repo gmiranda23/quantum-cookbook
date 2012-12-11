b5b85bda9b660757b2511a83a94baeb66240f3b0
Jeremy Hanmer <jeremy.hanmer@dreamhost.com>
2012-12-11 09:31:02 -0800

    nvp support

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jeremy@dreamhost.com"
 license          "Apache 2.0"
 description      "The OpenStack Networking service Quantum."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 
 %w{ ubuntu }.each do |os|
   supports os
--- recipes/plugin.rb
+++ recipes/plugin.rb
@@ -42,6 +42,25 @@ template "/etc/quantum/plugins.ini" do
             )
 end
 
+mysql_info = get_settings_by_role("mysql-master", "mysql")
+
+create_db_and_user("mysql",
+                    node["openstack"]["quantum"]["db"]["name"],
+                    node["openstack"]["quantum"]["db"]["username"],
+                    node["openstack"]["quantum"]["db"]["password"])
+
+template "/etc/quantum/plugins/nicira/nvp.ini" do
+  source "plugins/nicira/nvp.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    "mysql_pass" => node["openstack"]["quantum"]["db"]["password"],
+    "mysql_host" => mysql_info["bind_address"],
+    "mysql_user" => node["openstack"]["quantum"]["db"]["username"]
+  )
+end
+
 service "quantum-server" do
      action :restart
 end
